a455f74a732d11392d2f43e24060924f33747074
Kauã Maximino <kaua_maximino@outlook.com>
2022-09-19 15:29:46 -0300

    :recycle:	refactor: js for ts

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import express, {Request, Response} from 'express';
+import 'dotenv/config';
+
+const PORT = process.env.PORT || 3333;
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (_request: Request, response: Response) => {
+  return response.json('Hello World!');
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+})
