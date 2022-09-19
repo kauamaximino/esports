df745af2ab2445725401d1fc12940dc54f1295b7
Kauã Maximino <kaua_maximino@outlook.com>
2022-09-19 15:30:22 -0300

    :wrench:	chore: build tsc

--- server/build/server.js
+++ server/build/server.js
@@ -1,15 +1,11 @@
 import express from 'express';
 import 'dotenv/config';
-
 const PORT = process.env.PORT || 3333;
-
 const app = express();
 app.use(express.json());
-
 app.get('/', (_request, response) => {
-  return response.json('Hello World!');
-})
-
+    return response.json('Hello World!');
+});
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-})
+    console.log(`Server started on port ${PORT}`);
+});
