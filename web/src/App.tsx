ebce12418677ecdbd58b8fcac77da74e1099318d
Kauã Maximino <kaua_maximino@outlook.com>
2022-09-22 17:38:43 -0300

    style: :lipstick: add search icon

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import { MagnifyingGlassPlus } from 'phosphor-react'
 import './styles/main.css'
 import logogImg from './assets/logo-nlw-esports.png'
 
@@ -68,7 +69,8 @@ function App() {
           <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
           </div>
 
-          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded'>
+          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
+            <MagnifyingGlassPlus size={24} />
             Publicar anúncio
           </button>
         </div>
