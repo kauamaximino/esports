4d0fd788e4f0ec05624a183de9b6fa3c708543ed
Kauã Maximino <kaua_maximino@outlook.com>
2022-10-03 16:25:49 -0300

    feat: :sparkles: finish mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,17 +1,47 @@
-import { StatusBar } from 'react-native';
+import { useEffect, useRef } from "react";
+import { StatusBar } from "react-native";
 import {
   useFonts,
   Inter_400Regular,
   Inter_600SemiBold,
   Inter_700Bold,
   Inter_900Black,
-} from '@expo-google-fonts/inter';
+} from '@expo-google-fonts/inter'
+import { Subscription } from 'expo-modules-core';
+import * as Notifications from 'expo-notifications';
 
-import { Home } from './src/screens/Home';
-import { Loading } from './src/components/Loading'; 
-import { Background } from './src/components/Background';
+import { Routes } from './src/routes';
+import { Background } from "./src/components/Background";
+import { Loading } from "./src/components/Loading";
+
+import './src/service/notificationConfigs';
+import { getPushNotificationToken } from "./src/service/getPushNotificationToken";
 
 export default function App() {
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken()
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification)
+    });
+
+    responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response)
+    });
+
+    return () => {
+      if (getNotificationListener.current && responseNotificationListener.current) {
+        Notifications.removeNotificationSubscription(getNotificationListener.current);
+        Notifications.removeNotificationSubscription(responseNotificationListener.current);
+      }
+    }
+  }, []);
+
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
@@ -26,8 +56,8 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Home /> : <Loading/> }
+
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 }
-
